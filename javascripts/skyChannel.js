000ace8d03517f3c06722bd5e9d227de8cb8fe76
Mike Ralphson <mike.ralphson@gmail.com>
2016-07-13 14:24:29 +0100

    Films additional channels

---
+++ javascripts/skyChannel.js
@@ -0,0 +1,15 @@
+function skyChannel(c) {
+	if (c == '1020') return 'ITV';
+	if (c == '1073') return 'Film4';
+	if (c == '1621') return 'Channel4';
+	if (c == '2002') return 'BBC One';
+	if (c == '2006') return 'BBC Two';
+	if (c == '3605') return 'Horror';
+	if (c == '3708') return 'M4M';
+	if (c == '4058') return 'Channel 5';
+	if (c == '6532') return 'ITV2';
+	if (c == '6533') return 'ITV3';
+	if (c == '6534') return 'ITV4';
+}
+var channels = ['1020','1073','1621','2002','2006','3605','3708','4058','6532','6533','6534'];
+var strip = ['1020','1621','2002','2006','4058','6532','6533','6534'];
